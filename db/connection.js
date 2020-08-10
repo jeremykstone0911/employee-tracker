0416c77c23fe19fd5195786f3fa2cc5e03551527
jeremykstone0911 <61753461+jeremykstone0911@users.noreply.github.com>
2020-08-10 00:00:42 -0500

    connection.js modified

--- db/connection.js
+++ db/connection.js
@@ -6,8 +6,8 @@ const connection = mysql.createConnection({
   // Your username
   user: "root",
   // Your password
-  password: "",
-  database: "employees"
+  password: "password",
+  database: "employees",
 });
 
 connection.connect();
